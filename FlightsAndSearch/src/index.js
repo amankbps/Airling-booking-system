081a38e6c6ecfa7f328549443325466873efc57e
amankbps <amankbps@gmail.com>
2024-01-16 01:16:34 +0530

    completed basic sequlize setup for create a db

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 
-const {PORT}=require('../config/serverConfig.js');
+const {PORT}=require('./config/serverConfig.js');
 
 
 
